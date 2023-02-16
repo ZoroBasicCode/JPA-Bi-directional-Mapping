4b851bd65a3a855bd04cc58a09a8de3044983d5b
ZoroBasicCode <124671695+ZoroBasicCode@users.noreply.github.com>
2023-02-16 08:05:16 +0530

    Course.java

---
+++ .ManyToMany/Course.java
@@ -0,0 +1,53 @@
+package org.SRK;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String subject;
+	private int duriation;
+	@ManyToMany(mappedBy = "courses")
+	private List<Student> student;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public int getDuriation() {
+		return duriation;
+	}
+
+	public void setDuriation(int duriation) {
+		this.duriation = duriation;
+	}
+
+	public List<Student> getStudent() {
+		return student;
+	}
+
+	public void setStudent(List<Student> student) {
+		this.student = student;
+	}
+	
+}
