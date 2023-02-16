e54ac29d622e25540f72cb5ee59a4beb79c3ac17
ZoroBasicCode <124671695+ZoroBasicCode@users.noreply.github.com>
2023-02-16 22:35:53 +0530

    Branch.java

---
+++ .OneToMany/Branch.java
@@ -0,0 +1,52 @@
+package org.SRK;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Branch {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name,mail;
+	private long phon;
+	@ManyToOne
+	@JoinColumn(name = "hospital_id")
+	private Hospital hospital;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public long getPhon() {
+		return phon;
+	}
+	public void setPhon(long phon) {
+		this.phon = phon;
+	}
+	public Hospital getHospital() {
+		return hospital;
+	}
+	public void setHospital(Hospital hospital) {
+		this.hospital = hospital;
+	}
+	
+
+}
