b44262ccca97880dad25950c3bb32e8338ed897d
ZoroBasicCode <124671695+ZoroBasicCode@users.noreply.github.com>
2023-02-16 07:50:27 +0530

    Person.java

---
+++ .OneToOne/Person.java
@@ -0,0 +1,48 @@
+package org.SRK;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String state;
+	@OneToOne
+	@JoinColumn(name = "PanCard_id")
+	private PanCard pan;
+	
+	public PanCard getPan() {
+		return pan;
+	}
+	public void setPan(PanCard pan) {
+		this.pan = pan;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+
+}
