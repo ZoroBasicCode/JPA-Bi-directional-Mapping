9ae9100c2ae74e6e948910874842b4751f6dade0
ZoroBasicCode <124671695+ZoroBasicCode@users.noreply.github.com>
2023-02-16 08:04:29 +0530

    Student.java

---
+++ .ManyToMany/Student.java
@@ -0,0 +1,58 @@
+package org.SRK;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private long phon;
+	private double perc;
+	@ManyToMany
+	@JoinTable(name="Student_course" , joinColumns =   {@JoinColumn(name="Student_id")},
+	inverseJoinColumns = {@JoinColumn(name="Course_id")})	
+	private List<Course> courses;
+	
+	public List<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhon() {
+		return phon;
+	}
+	public void setPhon(long phon) {
+		this.phon = phon;
+	}
+	public double getPerc() {
+		return perc;
+	}
+	public void setPerc(double perc) {
+		this.perc = perc;
+	}
+	
+}
